cebda85ff534cff4a2997d284833edfb1dd98307
cssbreno <brenosoares.mygameacc@gmail.com>
2025-06-18 10:33:00 -0300

    refactor: remove try-except and streamline game logic for palavra secreta

--- logica/exercicios-basicos.py/jogo-palavra-secreta.py
+++ logica/exercicios-basicos.py/jogo-palavra-secreta.py
@@ -2,22 +2,32 @@ iniciar_jogo = input('Vamos jogar? (S)im / (N)ão: ').lower().startswith("s")
 
 palavra_secreta = 'Python'
 
-try:
-    if iniciar_jogo is True:
-        print('No jogo, você deve tentar adivinhar a palavra secreta. Vamos lá!')
-        print('------------------------')
+if iniciar_jogo is True:
+    print('No jogo, você deve tentar adivinhar a palavra secreta. Vamos lá!')
+    print('------------------------')
+    
+    palavra_oculta = ['*' for _ in palavra_secreta]
+    print('A palavra secreta é: ' + ''.join(palavra_oculta))
+
+
+    while True:
         
-        palavra_oculta = ['*' for _ in palavra_secreta]
-        print('A palavra secreta é: ' + ''.join(palavra_oculta))
+        chute_do_usuario = input('Digite uma letra: ').lower()
 
-
-        while True:
-            chute_do_usuario = input('Digite uma letra: ').lower()
-            for i,letra in enumerate(palavra_secreta):
-                if chute_do_usuario == letra.lower():
-                    palavra_oculta[i] = letra
-                    print('A palavra secreta é: ' + ''.join(palavra_oculta))
+        ## Valida quantidade de letras digitadas
+        if len(chute_do_usuario) > 1:
+            print('Digite apenas uma letra.')
             continue
 
-except ValueError:
-    print('Tudo bem. Até a próxima!')
+        if '*' not in palavra_oculta:
+            print('Você descobriu a palavra')
+            break
+        
+
+        for i,letra in enumerate(palavra_secreta):
+            if chute_do_usuario == letra.lower():
+                palavra_oculta[i] = letra
+                print('A palavra secreta é: ' + ''.join(palavra_oculta))
+        continue
+else:
+    print('Tudo bem, até a próxima!')
